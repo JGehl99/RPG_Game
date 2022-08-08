fdc678b7c3cf5f79b1cfd325a2f882f8fd65054b
Joshua Gehl <jgehl99@gmail.com>
2022-08-08 01:05:50 -0400

    Added Tilesets

---
+++ assets/maps/TilesetWater.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="TilesetWater" tilewidth="16" tileheight="16" tilecount="476" columns="28">
+ <image source="../NinjaAdventure/NinjaAdventure/Backgrounds/Tilesets/TilesetWater.png" width="448" height="272"/>
+</tileset>
