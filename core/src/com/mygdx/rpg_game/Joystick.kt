7343a26356df85950114e9778ee23cfa50eab855
Joshua Gehl <jgehl99@gmail.com>
2022-08-09 11:56:40 -0400

    Updated Joystick to rely on vector math instead of pixel math, added joystick on screen for visual representation of direction.

--- core/src/com/mygdx/rpg_game/Joystick.kt
+++ core/src/com/mygdx/rpg_game/Joystick.kt
@@ -1,8 +1,11 @@
 package com.mygdx.rpg_game
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputMultiplexer
 import com.badlogic.gdx.InputProcessor
-import com.badlogic.gdx.math.MathUtils.atan2
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
 
 class Joystick(private val player:Player): InputProcessor {
@@ -11,17 +14,22 @@ class Joystick(private val player:Player): InputProcessor {
     private var origPoint: Vector2 = Vector2()
     private var currentPoint: Vector2 = Vector2()
     private var directionVec: Vector2 = Vector2()
+    private var shapeRenderer: ShapeRenderer = ShapeRenderer()
+
+    fun addInputProcessors(multiplexer: InputMultiplexer){
+        multiplexer.addProcessor(this)
+    }
 
     override fun keyDown(keycode: Int): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
     override fun keyUp(keycode: Int): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
     override fun keyTyped(character: Char): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
@@ -33,7 +41,7 @@ class Joystick(private val player:Player): InputProcessor {
     override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
         isTouched = false
 
-        player.state = player.stateIdle
+        // player.state = player.stateIdle
         player.velocity.x = 0f
         player.velocity.y = 0f
 
@@ -45,39 +53,65 @@ class Joystick(private val player:Player): InputProcessor {
 
             currentPoint = Vector2(screenX.toFloat(), screenY.toFloat())
 
-            directionVec = currentPoint.sub(origPoint)
+            directionVec = currentPoint.cpy().sub(origPoint)
             directionVec.nor()
 
-            //Gdx.app.log("INFO", "Direction Vector: ${directionVec}")
-            player.velocity.x = directionVec.x * 16f
-            player.velocity.y = -directionVec.y * 16f
+            player.velocity.x = directionVec.x * 16f * 5
+            player.velocity.y = -directionVec.y * 16f * 5
 
-            Gdx.app.log("INFO", "Player Velocity: ${player.velocity.nor()}")
+            // Gdx.app.log("INFO", "Player Velocity: ${player.velocity}")
 
-            val angle = atan2(player.velocity.x, player.velocity.y) * 180 / 3.14159f
+            val a = Vector2(1f, 1f)
+            val b = Vector2(1f, -1f)
 
-            if (-135 < angle && angle <= -45) {
-                player.state = player.stateLeft
-            }
-            if (45 < angle && angle <= 135) {
-                player.state = player.stateRight
-            }
-            if ((135 < angle && angle <= 180) || (-180 < angle && angle <= -135)) {
-                player.state = player.stateDown
-            }
-            if (-45 < angle && angle <= 45) {
-                player.state = player.stateUp
+            val aDotDir = a.dot(directionVec)
+            val bDotDir = b.dot(directionVec)
+
+            when (true) {
+                (aDotDir >= 0f && bDotDir >=0) -> {
+                    player.state = player.stateRight
+                }
+                (aDotDir >= 0f && bDotDir < 0) -> {
+                    player.state = player.stateDown
+                }
+                (aDotDir < 0f && bDotDir >= 0) -> {
+                    player.state = player.stateUp
+                }
+                (aDotDir < 0f && bDotDir < 0) -> {
+                    player.state = player.stateLeft
+                }
             }
         }
-
         return true
     }
 
     override fun mouseMoved(screenX: Int, screenY: Int): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
     override fun scrolled(amountX: Float, amountY: Float): Boolean {
-        TODO("Not yet implemented")
+        return true
+    }
+
+    fun render() {
+        if(isTouched) {
+
+            val largeCircleRadius = 200f
+            val smallCircleRadius = 50f
+            val maxDist = largeCircleRadius - smallCircleRadius/2f
+
+            val relativeSmallCircleVector2 = Vector2(currentPoint.x - origPoint.x, currentPoint.y - origPoint.y).clamp(-maxDist, maxDist)
+
+            Gdx.gl.glEnable(GL20.GL_BLEND)
+            Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA)
+
+            shapeRenderer.color = Color(0.5f, 0.5f, 0.5f, 0.4f)
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
+            shapeRenderer.circle(origPoint.x, (Gdx.graphics.height.toFloat() - origPoint.y), 200f)
+            shapeRenderer.color = Color(0.1f, 0.1f, 0.1f, 0.5f)
+            shapeRenderer.circle(origPoint.x + relativeSmallCircleVector2.x, (Gdx.graphics.height.toFloat() - (origPoint.y + relativeSmallCircleVector2.y)), 50f)
+            shapeRenderer.end()
+            Gdx.gl.glDisable(GL20.GL_BLEND)
+        }
     }
 }
