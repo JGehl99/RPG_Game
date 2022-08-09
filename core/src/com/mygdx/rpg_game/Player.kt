0cb8faa368b62e6a4c048149663fb9537da94be9
Joshua Gehl <jgehl99@gmail.com>
2022-08-09 11:55:49 -0400

    Updated Player to include states and multiple animations.

--- core/src/com/mygdx/rpg_game/Player.kt
+++ core/src/com/mygdx/rpg_game/Player.kt
@@ -1,14 +1,11 @@
 package com.mygdx.rpg_game
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.files.FileHandle
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Matrix4
 import com.badlogic.gdx.math.Vector2
-
-
-
 
 /**
  * Simple Player class consisting of x, y, and sprite.
@@ -16,16 +13,16 @@ import com.badlogic.gdx.math.Vector2
  * @param x x coordinate
  * @param y y coordinate
  *
- * @property sprite Holds Player's [Sprite]
- * @property spriteBatch [SpriteBatch] for rendering Player's [Sprite]
- *
  * @author Joshua Gehl
  */
 
 class Player(var x: Float = 0f, var y: Float = 0f) {
 
-    private var sprite: Sprite = Sprite(Texture(Gdx.files.internal("7soulsrpggraphics_sprites_V3/RPG Graphics Pack - Sprites/Human (Front)/Full/player_85.png")))
+    private var animations: HashMap<String, Anim> = HashMap()
+
     private var spriteBatch: SpriteBatch = SpriteBatch()
+
+    private var currentAnimation: Anim
 
     val stateIdle = 0
     val stateLeft = 1
@@ -39,6 +36,18 @@ class Player(var x: Float = 0f, var y: Float = 0f) {
 
     var velocity = Vector2()
 
+    init {
+        val files: List<FileHandle> = Gdx.files.internal("anim/player/").list().filter {
+            it.extension() == "atlas"
+        }
+        for (file: FileHandle in files) {
+            animations[file.nameWithoutExtension()] = Anim(file.name(), 1/10f)
+            Gdx.app.log("INFO", "Created Animation  ${file.nameWithoutExtension()}")
+        }
+
+        currentAnimation = animations["idle"]!!
+    }
+
     /**
      * Updates Player's coordinates and draws the sprite
      *
@@ -51,9 +60,18 @@ class Player(var x: Float = 0f, var y: Float = 0f) {
         this.y += velocity.y * Gdx.graphics.deltaTime
         // Gdx.app.log("Info", "State: $state")
 
+        currentAnimation = when(state) {
+            0 -> animations["idle"]!!
+            1 -> animations["walkLeft"]!!
+            2 -> animations["walkRight"]!!
+            3 -> animations["walkUp"]!!
+            4 -> animations["walkDown"]!!
+            else -> animations["idle"]!!
+        }
+
         spriteBatch.projectionMatrix = combined
         spriteBatch.begin()
-        spriteBatch.draw(sprite, this.x - sprite.width/2, this.y - sprite.height/2)
+        spriteBatch.draw(currentAnimation.getNextFrame(), this.x - 8, this.y - 8)
         spriteBatch.end()
     }
 
